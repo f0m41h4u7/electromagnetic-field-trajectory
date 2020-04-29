58f704e42503492c500978904df8f6ac52e147f8
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-04-29 11:20:40 +0300

    pictures

---
+++ README.md
@@ -0,0 +1,11 @@
+## Electromagnetic field
+
+![alt text](https://raw.githubusercontent.com/f0m41h4u7/electromagnetic-field-trajectory/master/electromagnetic.png)
+
+## Electric field only
+
+![alt text](https://raw.githubusercontent.com/f0m41h4u7/electromagnetic-field-trajectory/master/electric.png)
+
+## Magnetic field only
+
+![alt text](https://raw.githubusercontent.com/f0m41h4u7/electromagnetic-field-trajectory/master/magnetic.png)
--- variables.go
+++ variables.go
@@ -3,13 +3,12 @@ package main
 import "math"
 
 var (
-	E  = 2300.0                // electric field strength, in v/m
-	B  = 0.01                  // induction, in teslas
-	q  = 1.6 * math.Pow10(-19) // charge, in coulombs
-	m  = 9.1 * math.Pow10(-31) // mass, in kg
-	dt = math.Pow10(-10)       // time step, in s
-	v  = math.Pow10(5)         // initial velocity, in m/s
-
-	omega = q * B / m     // Larmor fequency, in herz
-	r     = m * v / q / B // Larmor radius, in m
+	E     = 2300.0                // electric field strength, in v/m
+	B     = 0.01                  // induction, in teslas
+	q     = 1.6 * math.Pow10(-19) // charge, in coulombs
+	m     = 9.1 * math.Pow10(-31) // mass, in kg
+	dt    = math.Pow10(-10)       // time step, in s
+	v     = math.Pow10(5)         // initial velocity, in m/s
+	omega = q * B / m             // Larmor fequency, in herz
+	r     = m * v / q / B         // Larmor radius, in m
 )
